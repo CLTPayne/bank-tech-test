5c37181ad2ee90e167e82bdb375b3423cd40352e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-16 13:00:15 +0100

    Fix error in feature test Account initialization
    * new can not be called inside an it block

--- spec/features/view_statement_spec.rb
+++ spec/features/view_statement_spec.rb
@@ -2,8 +2,9 @@ require 'account'
 
 describe 'View bank statement feature' do
 
+  subject(:account) { Account.new }
+
   it 'prints a tidy history of transactions' do
-    account = new Account
     account.deposit(1000, "10/01/2012")
     account.deposit(2000, "13/01/2012")
     account.withdraw(500, "14/01/2012")
