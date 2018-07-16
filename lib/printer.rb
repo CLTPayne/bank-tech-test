3b2c16a04fb49ae1b6f557e920c83544b3d26925
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-16 22:34:28 +0100

    Display account statement
    * Printer class method to return transactions in reverse order and give
    a header

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,3 +1,28 @@
 class Printer
 
-end 
+  def self.display_statement(account)
+    statement = [header]
+    account.transactions.reverse_each do |transaction|
+      statement.push(to_string(transaction))
+    end
+    statement.join("\n")
+  end
+
+  def self.header
+    "date || credit || debit || balance"
+  end
+
+  def self.to_string(hash)
+    array = hash.map do |_key, value|
+      if value === 0
+        " "
+      elsif value.kind_of? Integer
+        " " + '%.2f' % value + " "
+      else
+        value + " "
+      end
+    end
+    array.join("||")
+  end
+
+end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -8,4 +8,34 @@ describe Printer do
     end
   end
 
+  describe '.display_statement' do
+
+    transaction_log = [
+      { date: "10/01/2012",
+        credit: 1000,
+        debit: 0,
+        balance: 1000
+      },
+      {
+        date: "13/01/2012",
+          credit: 2000,
+          debit: 0,
+          balance: 3000
+      },
+      { date: "14/01/2012",
+        credit: 0,
+        debit: 500,
+        balance: 2500
+      },
+    ]
+
+    it 'displays a tidy history of account transactions' do
+      account = double('account', :balance => 2500, :transactions => transaction_log)
+      statement =
+      "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 "
+      expect(Printer.display_statement(account)).to eql(statement)
+    end
+
+  end
+
 end
