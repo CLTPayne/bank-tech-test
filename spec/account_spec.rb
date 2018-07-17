f28428c055754549f9691ecc3684018d6ae8d1a1
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-17 11:31:47 +0100

    Remove class initialization tests
    * Remove now superfluous tests to ensure a class responds to initialise

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,12 +4,6 @@ describe Account do
 
   subject(:account) { described_class.new }
 
-  context 'default #initialization' do
-    it 'can create an accout' do
-      expect { Account.new }.not_to raise_error
-    end
-  end
-
   describe '#deposit' do
     it 'adds a sum to the account balance' do
       account.deposit(1000, "10/01/2012")
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,12 +2,6 @@ require 'printer'
 
 describe Printer do
 
-  context 'default #initialization' do
-    it 'can create a new printer object' do
-      expect { Printer.new }.not_to raise_error
-    end
-  end
-
   describe '.header' do
     it 'returns a statement header' do
       expect(Printer.header).to eql("date || credit || debit || balance")
