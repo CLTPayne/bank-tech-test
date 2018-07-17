ac1cf840dd4eed1dc2958362e79ac23df1aaea7a
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-17 11:18:19 +0100

    Refactor feature test to accept account history
    * Update test expectaction to fit account history being an object
    injected into account

--- spec/features/view_statement_spec.rb
+++ spec/features/view_statement_spec.rb
@@ -10,7 +10,7 @@ describe 'View bank statement feature' do
     account.withdraw(500, "14/01/2012")
     statement =
       "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 "
-    expect(Printer.display_statement(account)).to eql(statement)
+    expect(Printer.display_statement(account.account_history)).to eql(statement)
   end
 
 end
