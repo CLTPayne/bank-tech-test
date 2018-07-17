38b536ee36c60be7fb6cf4480cd6b5e393f2f8f4
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-17 16:12:16 +0100

    Refactor Account tests to for injected dependency
    * Remove testing of injected dependencies behaviour which is already
    tested else where

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,8 @@ require 'account'
 
 describe Account do
 
-  subject(:account) { described_class.new }
+  subject(:account) { described_class.new(account_history) }
+  let(:account_history) { spy }
 
   describe '#deposit' do
     it 'adds a sum to the account balance' do
@@ -11,22 +12,8 @@ describe Account do
     end
 
     it 'adds a transaction to the log' do
-      amount = 1000
-      date = "10/01/2012"
-      type = :deposit
-      balance = 1000
-      account_history = double('account_history', :amount => amount, :date => date, :type => type, :balance => balance)
       account.deposit(1000, "10/01/2012")
-      transaction_log = [
-        {
-          date: "10/01/2012",
-          credit: 1000,
-          debit: 0,
-          balance: 1000
-        }
-      ]
-      allow(account_history).to receive(:add_transaction).once.and_return(transaction_log)
-      expect(account.account_history.transactions).to eql(transaction_log)
+      expect(account_history).to have_received(:add_transaction).once
     end
 
   end
@@ -43,26 +30,8 @@ describe Account do
     end
 
     it 'adds a transaction to the log' do
-      amount = 500
-      date = "14/01/2012"
-      type = :withdraw
-      balance = 500
-      account_history = double('account_history', :amount => amount, :date => date, :type => type, :balance => balance)
       account.withdraw(500, "14/01/2012")
-      transaction_log = [
-        { date: "10/01/2012",
-          credit: 1000,
-          debit: 0,
-          balance: 1000
-        },
-        { date: "14/01/2012",
-          credit: 0,
-          debit: 500,
-          balance: 500
-        },
-      ]
-      allow(account_history).to receive(:add_transaction).once.and_return(transaction_log)
-      expect(account.account_history.transactions).to eql(transaction_log)
+      expect(account_history).to have_received(:add_transaction).twice
     end
 
   end
